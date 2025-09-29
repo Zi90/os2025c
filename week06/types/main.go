d364f981eb83e661331127ac5ef007b077a348f9
Zi90 <edwin03222@gmail.com>
2025-09-29 17:51:38 +0900

    v0.5 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -14,20 +14,20 @@ func main() {
 	// name = "Kim Inha"
 	// gpa = 3.99
 
-	// var id = 999
-	// var name = "Kim Inha"
-	// var gpa = 3.99
-
 	// var id int16 = 999
 	// var name string = "Kim Inha"
 	// var gpa float32 = 3.99
 
-	id := 999
-	name := "Kim Inha"
-	gpa := 3.99
+	// var id = 999
+	// var name = "Kim Inha"
+	// var gpa = 3.99
 
-	fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(gpa))
-	fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
+	// id := 999
+	// name := "Kim Inha"
+	// gpa := 3.99
+
+	// fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(gpa))
+	// fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
 	// fmt.Println(reflect.TypeOf(2.31))
 	// fmt.Println(reflect.TypeOf("Kim Inha"))
 	// fmt.Println(reflect.TypeOf(true))
@@ -38,4 +38,17 @@ func main() {
 	// fmt.Println(strings.Title("go. developer!"))
 	// fmt.Println("Kim\nInha\t\"\\")
 	// fmt.Println('2', '가')
+
+	// Zero value
+	var f64 float64
+	var t bool
+	var s string
+	var i int16
+	var i16 int16
+
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(t, reflect.TypeOf(t))
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(i, reflect.TypeOf(i))
+	fmt.Println(i16, reflect.TypeOf(i16))
 }
