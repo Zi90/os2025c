a88083045c39442b3333ceeb2c88276a25421bcd
Zi90 <edwin03222@gmail.com>
2025-11-03 17:21:39 +0900

    week10 package v0.3

---
+++ week10/cmd/ch04-ex04/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "week10/pkg/greeting"
+
+//import "greeting" // custom package(standard path)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+	deutsch.GutenTag()
+	korean.Anyung()
+}
