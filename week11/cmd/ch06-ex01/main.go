69ccb41f7933d474d52f3d5957a7ba9630c12399
Zi90 <edwin03222@gmail.com>
2025-11-10 17:28:12 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	// var subjects []string
 	// subjects = make([]string, 3)
-	subjects := make([]string, 3)
-	subjects[0] = "Go"
-	subjects[2] = "Python"
+	// subjects := make([]string, 3)
+	// subjects[0] = "Go"
+	// subjects[2] = "Python"
+	subjects := []string{"Go", "", "Python"}
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
