bca5775eaa72344f0d857154ebf97991615ad1da
Zi90 <edwin03222@gmail.com>
2025-11-10 17:35:01 +0900

    week11 slice v0.4

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -8,9 +8,13 @@ func main() {
 	// subjects := make([]string, 3)
 	// subjects[0] = "Go"
 	// subjects[2] = "Python"
-	subjects := []string{"Go", "", "Python"}
-
+	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[1:3] //slicing
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("============")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
